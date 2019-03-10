7646d92cc278e7b5c35ff430ccc9ca539fb52c05
ravildgar <ravildgar@gmail.com>
2019-03-10 17:16:22 +0300

    create SelectableColor's view and logic

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SelectableColor
+      backgroundColor="rgb(201, 76, 76)"
+      сorrectСolor="rgb(201, 76, 76)"
+      :key="'item.id'"
+    />
+    <SelectableColor
+      backgroundColor="rgb(231, 06, 76)"
+      сorrectСolor="rgb(201, 76, 76)"
+      :key="'item.id231'"
+    />
+    <SelectableColor
+      backgroundColor="rgb(1, 76, 76)"
+      сorrectСolor="rgb(201, 76, 76)"
+      :key="'item.i13d'"
+    />
+    <HelloI18n/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloI18n from "./components/HelloI18n.vue";
+import SelectableColor from "./components/SelectableColor.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloI18n,
+    SelectableColor
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SelectableColor.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="scene scene--card">
+    <div class="card" @click="giveAnswer" :class="{flipped: isClicked}">
+      <div class="card-face" :style="{ backgroundColor: backgroundColor }"></div>
+      <div class="card-face card-face-back">
+        <h1 v-if="isRightColor">{{$t("correctAnswer")}}</h1>
+        <h1 v-else>{{$t("incorrectAnswer")}}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      isClicked: false
+    };
+  },
+  props: {
+    // принимает цвет
+    backgroundColor: {
+      type: String,
+      required: true
+    },
+    сorrectСolor: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isRightColor: function() {
+      if (this.backgroundColor === this.сorrectСolor) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  methods: {
+    giveAnswer: function() {
+      this.isClicked = true;
+      if (this.isRightColor) {
+        console.log("угадал");
+      } else {
+        console.log("не угадал");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.scene {
+  width: 200px;
+  height: 200px;
+  border: 1px solid #ccc;
+  margin: 40px 0;
+  perspective: 600px;
+}
+.card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  transform-style: preserve-3d;
+  transform-origin: center right;
+  transition: transform 1s;
+}
+.flipped {
+  transform: translateX(-100%) rotateY(-180deg);
+}
+.card-face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+  backface-visibility: hidden;
+}
+
+.card-face-back {
+  background: blue;
+  transform: rotateY(180deg);
+}
+h1 {
+  margin-top: 45%;
+  font-size: 20px;
+}
+</style>
+
+<i18n>
+{
+  "en": {
+    "correctAnswer": "That's right!",
+    "incorrectAnswer": "Try again!"
+  },
+  "ru": {
+    "correctAnswer": "Угадано!",
+    "incorrectAnswer": "Попробуй еще!"
+  }
+}
+</i18n>
